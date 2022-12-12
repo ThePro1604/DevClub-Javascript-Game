a80660f1c6677328d93fc4e3e0e4fe11b1b20ce8
ThePro1604 <shahaf.sto@gmail.com>
2022-12-12 14:09:50 +0200

    board is complete (#1)

--- Monster.js
+++ Monster.js
@@ -1,8 +1,29 @@
+const monsterList = [
+    "./assets/monsters/monster1.jpeg",
+    "./assets/monsters/monster2.jpeg",
+    "./assets/monsters/monster3.jpeg",
+    "./assets/monsters/monster4.jpeg",
+    "./assets/monsters/monster5.jpeg",
+    "./assets/monsters/monster6.jpeg",
+];
+
+function chooseMonster() {
+    let randomNum = Math.floor(Math.random() * monsterList.length);
+    return (monsterList[randomNum])
+}
+
+
 function monsterGenerator(monsters) {
     for (let cell of monsters){
         let hostile = document.getElementById(cell);
+        hostile.setAttribute("class", cell+"-monster");
+
         hostile.innerHTML = "?";
-        hostile.style.backgroundColor = "red";
+        hostile.style.backgroundImage = "url(" + '"' + chooseMonster() + '"' + ")";
+        hostile.style.backgroundSize= "contain";
+        hostile.style.backgroundRepeat= "no-repeat";
+        hostile.style.backgroundPosition= "50% 50%";
+
     }
 
 }
--- Player.js
+++ Player.js
@@ -1,7 +1,7 @@
 function playerInteraction(previous, current) {
     let previousCell = document.getElementById(`cell-${previous[0]}-${previous[1]}`);
     let currentCell = document.getElementById(`cell-${current[0]}-${current[1]}`);
-    previousCell.style.backgroundColor = "white";
+    previousCell.style.backgroundColor = "Transparent";
     currentCell.style.backgroundColor = "yellow";
 
 }
